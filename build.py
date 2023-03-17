0e3748b5c8a397e036358114b95a73bf595f03f0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-03-17 13:51:06 +0100

    oops

--- build.py
+++ build.py
@@ -1,16 +1,27 @@
 import asyncio
 from json import load
+from textwrap import dedent
 
 repos = load(open("./repos.json", "r", encoding="utf-8"))
 
 async def gradlew(project):
-    proc = await asyncio.create_subprocess_shell(f"""
+    proc = await asyncio.create_subprocess_shell(dedent(f"""
         cd {project}
         chmod +x gradlew
         ./gradlew publishToMavenLocal
-    """)
+    """),
+     stdout=asyncio.subprocess.PIPE,
+    stderr=asyncio.subprocess.PIPE)
+    
+    stdout, stderr = await proc.communicate()
+    
+    print(f"[{project} exited with {proc.returncode}]")
+    if stdout:
+        print(f"[{project}] stdout:\n{stdout.decode()}")
+    if stderr:
+        print(f"[{project}] stdout:\n{stderr.decode()}")
     
 async def main():
     await asyncio.gather(*[
-        gradlew(repo) for repo in repos['gradlew']
+        gradlew(repo) for repo in repos["gradlew"]
     ])
